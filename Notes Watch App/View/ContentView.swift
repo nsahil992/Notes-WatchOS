1dace5c5ab6fa11feb495465a9f6646daa493597
Sahil Naik <nsahil992@gmail.com>
2024-09-18 12:03:36 +0530

    feat: List storage

--- Notes Watch App/View/ContentView.swift
+++ Notes Watch App/View/ContentView.swift
@@ -13,8 +13,33 @@ struct ContentView: View {
     @State private var text: String = ""
     
     func save() {
-        dump(notes)
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            try data.write(to: url)
+            
+        } catch {
+            print("Saving data has failed!")
+        }
     }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                
+            }
+        }
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -40,9 +65,23 @@ struct ContentView: View {
                 }
                 Spacer()
                 
-                Text("\(notes.count)")
+                List {
+                    ForEach(0..<notes.count, id: \.self) { i in
+                        HStack {
+                            Capsule()
+                                .frame(width: 4)
+                                .foregroundColor(.accentColor)
+                            Text(notes[i].text)
+                                .lineLimit(1)
+                                .padding(.leading, 5)
+                        }
+                    }
+                }
             }
             .navigationTitle("Notes")
+            .onAppear(perform: {
+                load()
+            })
         }
     }
 }
