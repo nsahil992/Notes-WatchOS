bd78b132c06f2900b24168332393b34af2c691b5
Sahil Naik <nsahil992@gmail.com>
2024-09-19 12:40:41 +0530

    feat: Notes extended view

---
+++ Notes Watch App/View/DetailView.swift
@@ -0,0 +1,57 @@
+//
+//  DetailView.swift
+//  Notes Watch App
+//
+//  Created by Sahil on 19/09/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let note: Note
+    let count: Int
+    let index: Int
+    
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 3) {
+            HStack {
+                Capsule()
+                    .frame(height: 1)
+                Image(systemName: "note.text")
+                
+                Capsule()
+                    .frame(height: 1)
+            }.foregroundStyle(.accent)
+            
+            Spacer()
+            ScrollView(.vertical) {
+                Text(note.text)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+            }
+            Spacer()
+            
+            HStack(alignment: .center) {
+                Image(systemName: "gear")
+                    .imageScale(.large)
+                
+                Spacer()
+                
+                Text("\(count) / \(index + 1)")
+                
+                Spacer()
+                
+                Image(systemName: "info.circle")
+                    .imageScale(.large)
+            }.foregroundStyle(.secondary)
+        }
+    }
+}
+
+#Preview {
+    var sampleData: Note = Note(id: UUID(), text: "Hello, world")
+    DetailView(note: sampleData, count: 5, index: 1)
+}
